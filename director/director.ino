d38785c31b76aab030f75bcf516b98797a50882b
Jeff Rule <jrule@omicia.com>
2016-07-24 14:17:02 -0700

    change the play order for the Asian Art show.

--- director/director.ino
+++ director/director.ino
@@ -50,8 +50,10 @@ const unsigned int all_dancers = 0xFFFF;      // this is the address we use when
 
 // Mercury20 2016
 // unsigned int dance_order[]  = {SINGULARITY2, GRAVITY_CG, LIFE, DVNMOMENT, all_dancers};  // order to queue the dancers
-unsigned int dance_order[]  = {GRAVITY_CG, LEPTON2, LIFE, all_dancers};  // order to queue the dancers
+// unsigned int dance_order[]  = {GRAVITY_CG, LEPTON2, LIFE, all_dancers};  // order to queue the dancers
 //unsigned int dance_order[]  = {DVNMOMENT};  // order to queue the dancers
+unsigned int dance_order[]  = {GRAVITY_CG, LEPTON2, HEAVENNEARTH, all_dancers};  // Asian Art Museum
+
                                
 unsigned int num_dancers = sizeof(dance_order) / sizeof(unsigned int);
 unsigned int timeout_seconds = 40;           // number of seconds to wait before skipping to next dancer

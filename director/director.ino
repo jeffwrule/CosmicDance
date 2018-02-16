9a4269fff4b4a75da05a43b9fc61be97c37a4a2f
Jeff Rule <jrule@omicia.com>
2018-02-15 22:38:05 -0800

    add code for PPF clocks piece.

--- director/director.ino
+++ director/director.ino
@@ -24,6 +24,7 @@ const unsigned int all_dancers = 0xFFFF;      // this is the address we use when
 #define SSINGLUARITY   0x36                      // gravity circle up (aka String Singularity)
 #define SFLUID    0x38                      // single acuator piece AKA Spiritual Fluid
 #define LONGINGCLOUD 0x40                   // double linear acuator piece (for cloudgate).
+#define PASTPRESENTFUTURE 0x42              // three clocks
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
